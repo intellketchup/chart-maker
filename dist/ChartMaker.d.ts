88f4604e6c23b850d85f9658375ec1d279959dc3
Drxco <156522801+DrxcoDev@users.noreply.github.com>
2024-08-27 18:16:33 +0000

    COMMIT

--- dist/ChartMaker.d.ts
+++ dist/ChartMaker.d.ts
@@ -10,5 +10,13 @@ interface ChartOptionsWithType {
 }
 export declare class ChartMaker {
     static createChart(ctx: CanvasRenderingContext2D, chartOptions: ChartOptionsWithType): void;
+    /**
+    * Exporta el gráfico en el canvas a una imagen en formato PNG o JPEG.
+    * @param canvas - El elemento canvas que contiene el gráfico.
+    * @param format - El formato de la imagen. Puede ser 'image/png' o 'image/jpeg'.
+    * @param quality - La calidad de la imagen para el formato JPEG (entre 0 y 1). Ignorado para PNG.
+    * @returns La URL de la imagen en base64.
+    */
+    static exportChartToImage(canvas: HTMLCanvasElement, format: 'image/png' | 'image/jpeg', quality?: number): string;
 }
 export {};
